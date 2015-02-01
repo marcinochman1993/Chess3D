90603ad7ec41b20313e846208b5cacf85217b8cf
Marcin Ochman <marcinochman1993@wp.pl>
2015-02-01 15:22:18 +0100

    Usunięto niepotrzebną dokumentację - powinna generować się po clone

--- doc/html/search/classes_63.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['chessboard',['Chessboard',['../class_chessboard.html',1,'']]],
-  ['chessboardfield',['ChessboardField',['../class_chessboard_field.html',1,'']]],
-  ['chessboardfieldcoordinates',['ChessboardFieldCoordinates',['../class_chessboard_field_coordinates.html',1,'']]],
-  ['chesspiece',['ChessPiece',['../class_chess_piece.html',1,'']]],
-  ['chesspiecemovement',['ChessPieceMovement',['../class_chess_piece_movement.html',1,'']]]
-];
