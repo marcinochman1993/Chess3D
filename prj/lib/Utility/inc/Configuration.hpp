a99b95f632a0bea57f420edc450c53838317015a
Marcin Ochman <marcinochman1993@wp.pl>
2015-03-11 21:42:56 +0100

    Dodano header Configuration.hpp

---
+++ prj/lib/Utility/inc/Configuration.hpp
@@ -0,0 +1,16 @@
+/*
+ * Configuration.hpp
+ *
+ *  Created on: Feb 25, 2015
+ *      Author: mochman
+ */
+
+#ifndef PRJ_LIB_UTILITY_INC_CONFIGURATION_HPP_
+#define PRJ_LIB_UTILITY_INC_CONFIGURATION_HPP_
+
+
+#include "Configuration/ConfigurationManager.hpp"
+#include "Configuration/ConfigurationReader.hpp"
+
+
+#endif /* PRJ_LIB_UTILITY_INC_CONFIGURATION_HPP_ */
